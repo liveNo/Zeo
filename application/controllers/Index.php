5e6965ef031641cad7f3114b9db1e92583754725
xululu <xuyanlu0424@163.com>
2016-03-07 09:49:33 +0800

    Init Yaf index file

---
+++ application/controllers/Index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 首页Index
+ */
+class IndexController extends Yaf\Controller_Abstract {
+
+    public function indexAction () 
+    {
+        echo "Hello Yaf!";
+        return ;
+    }
+
+
+
+
+}
+
+
--- public/index.php
+++ public/index.php
@@ -1,10 +1,9 @@
 <?php
+
 define("APP_PATH", realpath(dirname(__FILE__) . '/../'));
 
 $app = new Yaf\Application(APP_PATH . "/config/application.ini");
 
 $app->run();
-
-
 
 
