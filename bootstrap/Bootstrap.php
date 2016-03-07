4b5d581b6fe0a00ad071e4b98a6f0028261ff9da
xululu <xuyanlu0424@163.com>
2016-03-07 11:12:34 +0800

    Bootstrap

---
+++ bootstrap/Bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+class Bootstrap extends Yaf\Bootstrap_Abstract {
+
+    public function _initConfig () 
+    {
+        $config = Yaf\Application::app()->getConfig();
+        Yaf\Registry::set("config", $config);
+    }
+
+}
+
+
+
