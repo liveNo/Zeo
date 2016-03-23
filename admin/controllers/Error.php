493f8d70b01415ec42b2aa8c94fc01b74c9886ef
xululu <xuyanlu0424@163.com>
2016-03-23 21:07:20 +0800

    Init Admin frame

---
+++ admin/controllers/Error.php
@@ -0,0 +1,12 @@
+<?php
+
+class ErrorController extends Yaf\Controller_Abstract {
+
+    public function errorAction ($exception)
+    {
+        assert($exception === $exception->getCode());
+        echo "Code:" . $exception->getCode() . " Message:" . $exception->getMessage();
+
+    }
+
+}
---
+++ admin/controllers/Index.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * IndexController 
+ * 
+ * @uses Yaf
+ * @package 
+ * @version $id$
+ * @copyright 1997-2005 The PHP Group
+ * @author 徐路路 <xululu@eventmosh.com> 
+ * @license 北京活动时文化传媒有限公司
+ */
+
+class IndexController extends Yaf\Controller_Abstract {
+
+    public function indexAction () 
+    {
+        $content = "Hello Yaf!";
+        $this->getView()->assign('content', $content);
+    }
+
+
+
+    public function testAction() 
+    {
+        echo "Hello Test";
+    }
+
+
+}
+
+
