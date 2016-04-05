5e30db5976aefc69ac5c3527abf14933785a840e
xululu <xuyanlu0424@163.com>
2016-04-05 20:59:52 +0800

    markdown init

---
+++ application/controllers/Mark.php
@@ -0,0 +1,22 @@
+<?php
+
+
+/**
+ * MarkController  Markdown use
+ * 
+ * @uses Yaf
+ * @package 
+ * @version $id$
+ * @copyright 1997-2005 The PHP Group
+ * @author 徐路路 <xululu@eventmosh.com> 
+ * @license 北京活动时文化传媒有限公司
+ */
+class MarkController extends Yaf\Controller_Abstract {
+
+    
+
+    public function indexAction() {}
+
+
+
+}
